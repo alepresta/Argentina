e1aa3ead873c3aac606103ecd91e83dead800ca9
alepresta <alejandro.modernizacion@gmail.com>
2018-10-02 11:52:24 -0300

    feat: hook ok

--- test/cucumber/features/support/hooks.rb
+++ test/cucumber/features/support/hooks.rb
@@ -1,3 +1,36 @@
+cucumber_features = [
+    {:fichero => "reporte.html" ,                       :tag => "reporte" ,                       :formato => "html",  :plataforma => "QA"},
+    {:fichero => "reporte.json" ,                       :tag => "reporte" ,                       :formato => "json",  :plataforma => "QA"},
+    {:fichero => "compartir.html" ,                     :tag => "compartir" ,                     :formato => "html",  :plataforma => "Argentina"},
+    {:fichero => "compartir.json" ,                     :tag => "compartir" ,                     :formato => "json",  :plataforma => "Argentina"},
+    {:fichero => "formularios.html" ,                   :tag => "formularios" ,                   :formato => "html",  :plataforma => "Argentina"},
+    {:fichero => "formularios.json" ,                   :tag => "formularios" ,                   :formato => "json",  :plataforma => "Argentina"},
+    {:fichero => "home.html" ,                          :tag => "home" ,                          :formato => "html",  :plataforma => "Argentina"},
+    {:fichero => "home.json" ,                          :tag => "home" ,                          :formato => "json",  :plataforma => "Argentina"},
+    {:fichero => "listado_ordenado.html" ,              :tag => "listado_ordenado" ,              :formato => "html",  :plataforma => "Argentina"},
+    {:fichero => "listado_ordenado.json" ,              :tag => "listado_ordenado" ,              :formato => "json",  :plataforma => "Argentina"},
+    {:fichero => "login_redes_sociales.html" ,          :tag => "login_redes_sociales" ,          :formato => "html",  :plataforma => "Argentina"},
+    {:fichero => "login_redes_sociales.json" ,          :tag => "login_redes_sociales" ,          :formato => "json",  :plataforma => "Argentina"},
+    {:fichero => "mas_visitados.html" ,                 :tag => "mas_visitados" ,                 :formato => "html",  :plataforma => "Argentina"},
+    {:fichero => "mas_visitados.json" ,                 :tag => "mas_visitados",                  :formato => "json",  :plataforma => "Argentina"},
+    {:fichero => "organismos.html" ,                    :tag => "organismos" ,                    :formato => "html",  :plataforma => "Argentina"},
+    {:fichero => "organismos.json" ,                    :tag => "organismos" ,                    :formato => "json",  :plataforma => "Argentina"},
+    {:fichero => "sube.html" ,                          :tag => "sube" ,                          :formato => "html",  :plataforma => "Argentina"},
+    {:fichero => "sube.json" ,                          :tag => "sube" ,                          :formato => "json",  :plataforma => "Argentina"},
+    {:fichero => "test.html" ,                          :tag => "test" ,                          :formato => "html",  :plataforma => "QA"},
+    {:fichero => "test.json" ,                          :tag => "test" ,                          :formato => "json",  :plataforma => "QA"},
+    {:fichero => "turnos.html" ,                        :tag => "turnos" ,                        :formato => "html",  :plataforma => "SNT"},
+    {:fichero => "turnos.json" ,                        :tag => "turnos" ,                        :formato => "json",  :plataforma => "SNT"},
+    {:fichero => "turnos_recepcionar.html" ,            :tag => "turnos_recepcionar" ,            :formato => "html",  :plataforma => "SNT"},
+    {:fichero => "turnos_recepcionar.json" ,            :tag => "turnos_recepcionar" ,            :formato => "json",  :plataforma => "SNT"},
+    {:fichero => "turnos_sacar.html" ,                  :tag => "turnos_sacar" ,                  :formato => "html",  :plataforma => "SNT"},
+    {:fichero => "turnos_sacar.json" ,                  :tag => "turnos_sacar" ,                  :formato => "json",  :plataforma => "SNT"},
+    {:fichero => "turnos_sacar_multiples.html" ,        :tag => "turnos_sacar_multiples" ,        :formato => "html",  :plataforma => "SNT"},
+    {:fichero => "turnos_sacar_multiples.json" ,        :tag => "turnos_sacar_multiples" ,        :formato => "json",  :plataforma => "SNT"},
+    {:fichero => "turnos_sacar_multiples_tod.html" ,    :tag => "turnos_sacar_multiples_tod" ,    :formato => "html",  :plataforma => "SNT"},
+    {:fichero => "turnos_sacar_multiples_tod.json" ,    :tag => "turnos_sacar_multiples_tod",     :formato => "json",  :plataforma => "SNT"},
+]
+
 
 Before do
   # @cukehub_api_key = "c1ty7uj72StwdqLGWKQZRjd8"
@@ -6,67 +39,42 @@ Before do
 end
 
 at_exit do
-  casos_prueba = [
-      {:reporte => "reporte.html" ,    :report_name => "reporte" , :formato => "html"},
-      {:reporte => "reporte.json" ,    :report_name => "reporte" , :formato => "json"},
-      {:reporte => "compartir.html" ,    :report_name => "compartir" , :formato => "html"},
-      {:reporte => "compartir.json" ,    :report_name => "compartir" , :formato => "json"},
-      {:reporte => "formularios.html" ,    :report_name => "formularios" , :formato => "html"},
-      {:reporte => "formularios.json" ,    :report_name => "formularios" , :formato => "json"},
-      {:reporte => "home.html" ,    :report_name => "home" , :formato => "html"},
-      {:reporte => "home.json" ,    :report_name => "home" , :formato => "json"},
-      {:reporte => "listado_ordenado.html" ,    :report_name => "listado_ordenado" , :formato => "html"},
-      {:reporte => "listado_ordenado.json" ,    :report_name => "listado_ordenado" , :formato => "json"},
-      {:reporte => "login_redes_sociales.html" ,    :report_name => "login_redes_sociales" , :formato => "html"},
-      {:reporte => "login_redes_sociales.json" ,    :report_name => "login_redes_sociales" , :formato => "json"},
-      {:reporte => "mas_visitados.html" ,    :report_name => "mas_visitados" , :formato => "html"},
-      {:reporte => "mas_visitados.json" ,    :report_name => "mas_visitados", :formato => "json" },
-      {:reporte => "organismos.html" ,    :report_name => "organismos" , :formato => "html"},
-      {:reporte => "organismos.json" ,    :report_name => "organismos" , :formato => "json"},
-      {:reporte => "sube.html" ,    :report_name => "sube" , :formato => "html"},
-      {:reporte => "sube.json" ,    :report_name => "sube" , :formato => "json"},
-      {:reporte => "test.html" ,    :report_name => "test" , :formato => "html"},
-      {:reporte => "test.json" ,    :report_name => "test" , :formato => "json"},
-      {:reporte => "turnos.html" ,    :report_name => "turnos" , :formato => "html"},
-      {:reporte => "turnos.json" ,    :report_name => "turnos" , :formato => "json"},
-      {:reporte => "turnos_recepcionar.html" ,    :report_name => "turnos_recepcionar" , :formato => "html"},
-      {:reporte => "turnos_recepcionar.json" ,    :report_name => "turnos_recepcionar" , :formato => "json"},
-      {:reporte => "turnos_sacar.html" ,    :report_name => "turnos_sacar" , :formato => "html"},
-      {:reporte => "turnos_sacar.json" ,    :report_name => "turnos_sacar" , :formato => "json"},
-      {:reporte => "turnos_sacar_multiples.html" ,    :report_name => "turnos_sacar_multiples" , :formato => "html"},
-      {:reporte => "turnos_sacar_multiples.json" ,    :report_name => "turnos_sacar_multiples" , :formato => "json"},
-      {:reporte => "turnos_sacar_multiples_tod.html" ,    :report_name => "turnos_sacar_multiples_tod" , :formato => "html"},
-      {:reporte => "turnos_sacar_multiples_tod.json" ,    :report_name => "turnos_sacar_multiples_tod", :formato => "json" }
-  ]
-  casos_prueba.each do |caso|
+  cucumber_features.each do |caso|
     fecha, hora = Time.now.strftime('%Y-%m-%d--%H-%M-%S').split("--")
-    reporte = caso[:reporte]
-    report_name = caso[:report_name]
+    fichero = caso[:fichero]
+    tag = caso[:tag]
     formato = caso[:formato]
-    #path_root_proyecto1 = "/var/lib/jenkins/workspace"
-    if File.exist? "#{path_root_proyecto}/Argentina/test/report/#{report_name}/#{reporte}"
-      html_content = IO.read("#{path_root_proyecto}/Argentina/test/report/#{report_name}/#{reporte}")
-      html_content.gsub!("<h1>Cucumber Features</h1>","<h1><span>Caso de Prueba:</span> (#{report_name}) <br>Realizado el día: #{fecha} a las: #{hora}</br></h1>")
-      nombre_del_archivo = "#{report_name}_#{fecha}-#{hora}.#{formato}"
-      html_content_nuevo = "#{path_root_proyecto}/Argentina/app/views/pages/#{nombre_del_archivo}"
-      #puts html_content
-      if html_content.include? 'step failed'
-        estado = "  ლ(ಠ益ಠლ) ERROR"
-      else
-        estado = " \\(• ◡ •)/ Sin Errores"
-      end
-      IO.write("#{html_content_nuevo}",html_content)
-      # --------------------------------------------------------------------------------------
-      pegar   = "<h1>Ejecuciones de pruebas</h1>
+    plataforma = caso[:plataforma]
+    if File.exist? "#{path_root_proyecto}/Argentina/test/report/constructor_de_reportes/#{fichero}"
+      if formato == "html"
+        html_content = IO.read("#{path_root_proyecto}/Argentina/test/report/constructor_de_reportes/#{fichero}")
+        html_content.gsub!("<h1>Plataforma: #{plataforma}</h1>","<h1><span>Caso de Prueba:</span> (#{tag}) <br>Realizado el día: #{fecha} a las: #{hora}</br></h1>")
+        nombre_del_archivo = "#{plataforma}_#{fichero}"
+        path_pages = "#{path_root_proyecto}/Argentina/app/views/pages/#{nombre_del_archivo}"
+        IO.write("#{path_pages}",html_content)
+        if html_content.include? 'step failed'
+          estado_texto = "  ლ(ಠ益ಠლ) ERROR"
+          estado = true
+        else
+          estado_texto = " \\(• ◡ •)/ Sin Errores"
+          estado = false
+        end
+        pegar   = "<h1>Ejecuciones de pruebas</h1>
                   QA231425267"
-      pegar_1 = "<p><%= link_to \"#{nombre_del_archivo}\", page_path(\"#{nombre_del_archivo}\") %><strong> \"#{estado}\"</strong></p>"
-      html_index = IO.read("#{path_root_proyecto}/Argentina/app/views/welcome/index.html.erb")
-      html_index.gsub!("<h1>Ejecuciones de pruebas</h1>","#{pegar}")
-      html_index_nuevo = "#{path_root_proyecto}/Argentina/app/views/welcome/index.html.erb"
-      IO.write("#{html_index_nuevo}",html_index)
-      html_index.gsub!("QA231425267","#{pegar_1}")
-      IO.write("#{html_index_nuevo}",html_index)
-      File.delete("#{path_root_proyecto}/Argentina/test/report/#{report_name}/#{reporte}")
+        pegar_1 = "<p><%= link_to \"#{nombre_del_archivo}\", page_path(\"#{nombre_del_archivo}\") %><strong> #{estado} ......... #{estado_texto}</strong></p>"
+        html_index = IO.read("#{path_root_proyecto}/Argentina/app/views/welcome/index.html.erb")
+        html_index.gsub!("<h1>Ejecuciones de pruebas</h1>","#{pegar}")
+        html_index_nuevo = "#{path_root_proyecto}/Argentina/app/views/welcome/index.html.erb"
+        IO.write("#{html_index_nuevo}",html_index)
+        html_index.gsub!("QA231425267","#{pegar_1}")
+        IO.write("#{html_index_nuevo}",html_index)
+        File.delete("#{path_root_proyecto}/Argentina/test/report/constructor_de_reportes/#{fichero}")
+      else
+        nombre_del_archivo = "#{plataforma}_#{tag}_#{fecha}_#{hora}.#{formato}"
+        File.rename "#{path_root_proyecto}/Argentina/test/report/constructor_de_reportes/#{fichero}", "#{path_root_proyecto}/Argentina/test/report/json_reportes/#{nombre_del_archivo}"
+      end
+
     end
+
   end
 end
